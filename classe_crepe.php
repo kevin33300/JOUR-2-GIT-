5e5ba0682f8f529e0913da5e588fef5ef897108d
kevin <kevinfrance17440@gmail.com>
2022-01-03 11:37:47 +0100

    👌 IMPROVE: mise à jour fichier class

--- classe_crepe.php
+++ classe_crepe.php
@@ -1,26 +1,31 @@
 <?php
 class Crêpe {
 
-    // ci dessous les paramètres/ou attributs qui vont servir à créer notre objet. 
+    // ci dessous les paramètres/ou attributs qui vont servir à créer notre objet. Il y en a des privés et il y en a des publiques. 
     public $oeuf;
     public $beurre;
     public $farine;
+    private $topping; // ici c'est un attribut privé
+    
     // ci dessous le constructeur qui est la fonction permettant de créer l'objet, elle a comme paramètres les variables créées ci dessus. IL Y A TOUJOURS DEUX UNDERSCORE SUR LA FONCTION CONSTRUCT
     public function __construct(
         $oeuf, // on aurait pu aussi mettre aliment 1 ici 
         $beurre,
         $farine,
+        $topping,
     ) {
         $this->oeuf = $oeuf; // donc on aurait mis ici $this->oeuf = $aliment1
         $this->beurre = $beurre;
         $this->farine = $farine;
+        $this->topping = $topping;
     }
 
     public function description_crepe () {
         echo "Il s'agit d'une crêpe avec : \n";
         echo $this->oeuf. " oeufs. \n";
-        echo $this->beurre. " oeufs. \n";
-        echo $this->farine. " oeufs. \n";
+        echo $this->beurre. " beurre. \n";
+        echo $this->farine. " farine. \n";
+        echo $this->topping. " topping. \n";
 
     }
 }
--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
 require './classe_crepe.php';
-$crepeauchocolat = new Crêpe (2,3,5);
+$crepeauchocolat = new Crêpe (2,3,5,[]); // ici on ne connait pas encore le topping donc on l'initialise en array puisque le topping constituera plusieurs éléments.
 var_dump($crepeauchocolat);
 echo $crepeauchocolat-> description_crepe();
